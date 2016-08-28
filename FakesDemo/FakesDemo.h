ee2a8d83b89a446b2dba9e567c671627ceb0813a
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-08-29 08:05:45 +1000

    New framework from Xcode template

---
+++ FakesDemo/FakesDemo.h
@@ -0,0 +1,19 @@
+//
+//  FakesDemo.h
+//  FakesDemo
+//
+//  Created by Giovanni on 29/08/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FakesDemo.
+FOUNDATION_EXPORT double FakesDemoVersionNumber;
+
+//! Project version string for FakesDemo.
+FOUNDATION_EXPORT const unsigned char FakesDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FakesDemo/PublicHeader.h>
+
+
